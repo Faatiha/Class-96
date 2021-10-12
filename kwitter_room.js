62af58de9ab612a1cf4e8a86370bb9a95602e7ac
Faatiha <66500499+Faatiha@users.noreply.github.com>
2021-10-12 16:31:35 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,59 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyBrSOQU-6V7JQs1zoKE0cOERyKw0l5tPcw",
+      authDomain: "kwitter-de760.firebaseapp.com",
+      databaseURL: "https://kwitter-de760-default-rtdb.firebaseio.com",
+      projectId: "kwitter-de760",
+      storageBucket: "kwitter-de760.appspot.com",
+      messagingSenderId: "179093654538",
+      appId: "1:179093654538:web:3a8a7b1244dffe1b4de3f3"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+//ADD YOUR FIREBASE LINKS HERE
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!!";
+
+function addRoom(){
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.js";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("Room name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirect(this.id)'>#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+
+function redirect(name){
+console.log(name);
+localStorage.setItem("room_name", name);
+window.location = "kwitter_page.html";
+}
+
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
+
+function send(){
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      })
+      document.getElementById("msg").value = "";
+}
+
